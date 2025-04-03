7b24d435d42ecd2b921c15db56e0b3d8c2d6c362
Eden <edenjtait@gmail.com>
2025-04-03 14:25:26 -0700

    removed redundant code

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
+from stats import word_count
+
 def get_book_text():
     with open("books/frankenstein.txt") as f:
         return f.read()
-
-def count_words(book):
-    count  = 0
-    words = book.split()
-    for w in words:
-        count += 1
-    return count
 
 def main():
     words = count_words(get_book_text())
