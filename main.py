0a305d72e424c5672e8fb816fd8424ffd8237ced
Eden <edenjtait@gmail.com>
2025-04-03 14:21:34 -0700

    count words in book

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ def get_book_text():
     with open("books/frankenstein.txt") as f:
         return f.read()
 
+def count_words(book):
+    count  = 0
+    words = book.split()
+    for w in words:
+        count += 1
+    return count
+
 def main():
-    print(get_book_text())
+    words = count_words(get_book_text())
+    print(f"{words} words found in the document")
 
 main()
